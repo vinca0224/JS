fc296ceabf339e92da584e25cd5d6f3f807921be
vinca0224 <vinca0224@naver.com>
2024-05-13 14:46:23 +0900

    0513

---
+++ day5/dom/className.js
@@ -0,0 +1,5 @@
+const liElement = document.getElementsByClassName("a");
+console.log(liElement);
+for(const li of liElement){
+    console.log(li.textContent);    
+}
---
+++ day5/dom/querySelector.js
@@ -0,0 +1,7 @@
+//li 첫번째 요소에 접근
+// document.querySelector("li").style.backgroundColor = 'skyblue';
+
+const btn = document.querySelector("#mybtn");
+btn.addEventListener("click", () => {
+    alert('button clicked');
+});
